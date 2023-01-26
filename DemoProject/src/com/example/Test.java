33467cd995a49bc4aa97e570d905b6f9270773ec
Utkarshpatil12 <uttupatil1211@gmail.com>
2023-01-26 15:44:26 +0530

    demo project created

---
+++ DemoProject/src/com/example/Test.java
@@ -0,0 +1,16 @@
+package com.example;
+
+public class Test {
+
+	public static void main(String[] args) {
+		
+		Employee emp=new Employee();
+		emp.setId(01);
+		emp.setName("Utkarsh");
+		emp.setAddress("Kolhapur");
+		
+		System.out.println(emp);
+
+	}
+
+}
